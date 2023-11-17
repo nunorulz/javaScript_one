d6bc4c4ab93700c6f808121e5e9d7ad7bc8959c3
Nuno Rocha <nunorulz@gmail.com>
2023-11-17 16:14:37 -0300

    Atividade Modulo 5.1

--- modulo5.1/script.js
+++ modulo5.1/script.js
@@ -6,7 +6,17 @@ let listaOl = document.getElementById('lista-ordenada');
 tituloInner.innerText = 'Titulo adicionado via innerText';
 linkInner.innerText = 'prozeducacao.com.br';
 
-listaOl.innerHTML =
-  '<li><a href="#">Lista ordenada criada via innerHTML</a></li>';
+for (let i = 0; i < 3; i++) {
+  const listaNaoOrdenada = document.createElement('li');
+  listaNaoOrdenada.innerHTML = `<a href="#">Lista ${i} não ordenada criada via innerHTML</a>`;
+  let ul = document.querySelector('ul');
+  ul.appendChild(listaNaoOrdenada);
+}
 
-listaUl.innerHTML = '<li>Lista não ordenada criada via innerHTML</li>';
+for (let i = 0; i < 3; i++) {
+  const listaOrdenada = document.createElement('li');
+  listaOrdenada.innerHTML = `<a href="#">Lista ${i} ordenada criada via innerHTML</a>`;
+
+  let ol = document.querySelector('ol');
+  ol.appendChild(listaOrdenada);
+}
