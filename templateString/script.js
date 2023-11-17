a9e5a3b74c681c2d943ac0d88aa587c92859e1d5
Nuno Rocha <nunorulz@gmail.com>
2023-11-17 16:59:26 -0300

    operações

---
+++ templateString/script.js
@@ -0,0 +1,7 @@
+let operacoes = (num1, num2) => {
+  console.log(`${num1} + ${num2} = ${num1 + num2}`);
+  console.log(`${num1} - ${num1} = ${num1 - num2}`);
+  console.log(`${num1} x ${num1} = ${num1 * num2}`);
+  console.log(`${num1} / ${num1} = ${num1 / num2}`);
+};
+operacoes(4, 5);
